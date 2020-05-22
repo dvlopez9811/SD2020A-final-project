79b4161e0cdeec943e7f2c2d0629f74d3982ccc6
ManuelCoral1998 <43486285+ManuelCoral1998@users.noreply.github.com>
2020-05-22 00:12:14 -0500

    Dockerfile api, docker-compese and some changes in the frontend and backend (change routes for backend and DB) (#4)

--- rest-api/app.js
+++ rest-api/app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const app = express();
 
 // connecting to db 
-mongoose.connect('mongodb://localhost:27017/ourdb', { useNewUrlParser: true })
+mongoose.connect('mongodb://mongo:27017/ourdb', { useNewUrlParser: true })
     .then(db => console.log('Db connected'))
     .catch(err => console.log(err));
 
