a36e20af620c7aa9123b7f84cef7c0db48286fc9
Sebastian Quintero <jsebastianquintero@gmail.com>
2020-05-24 11:56:42 -0500

    fixing test error

--- test/test.js
+++ test/test.js
@@ -9,33 +9,28 @@ describe('Api Test', () => {
     name: 'John'
   };
 
-//Get Front
-it('Consume GET Front Service', async () => {
+  //Get Front
+  it('Consume GET Front Service', async () => {
     const response = await agent.get('http://localhost:8080/');
     
-  
     expect(response.status).to.equal(statusCode.OK);
     expect(response.type).to.equal('text/html');
   });
+  
+  //Post 
+  it('Consume POST Back Service with query parameters', async () => {
+    const response = await agent.post('http://localhost:3000/add')
+    .query(query);
 
-//Post 
-it('Consume POST Back Service with query parameters', async () => {
-   
-   const response = await agent.post('http://localhost:3000/add').query(query).end( function(err,res){
-    
     expect(response.status).to.equal(statusCode.OK);
-
-    });
   });
+  
+  //GET Back  
+  it('Consume GET Back Service', async () => {
+    const response = await agent.get('http://localhost:3000/');
 
-//GET Back  
-it('Consume GET Back Service', async () => {
-   const response = await agent.get('http://localhost:3000/');
-   console.log(response.body)
-   expect(response.status).to.equal(statusCode.OK);
-   expect(response.body).to.not.equal('');
-   expect(response.body[0].name).to.equal(query.name);
-
-   //expect(response.body).to.have.property('origin');
- });
+    expect(response.status).to.equal(statusCode.OK);
+    expect(response.body).to.not.equal('');
+    expect(response.body[0].name).to.equal(query.name);
+  });
 });
