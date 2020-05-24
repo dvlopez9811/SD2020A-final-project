7194c2326292d376646373ddd611b68f3ebc473d
Sebastian Quintero <jsebastianquintero@gmail.com>
2020-05-24 13:28:46 -0500

    Add swagger documentation

--- rest-api/routes/index.js
+++ rest-api/routes/index.js
@@ -3,11 +3,63 @@ const router = express.Router();
 
 const Person = require('../models/person');
 
+/**
+ * @swagger
+ * /:
+ *  get:
+ *      summary: Retorna una lista de todos los usuarios
+ *      description: Retorna una lista de todos los usuarios
+ *      responses:
+ *          '200':
+ *              description: Respuesta exitosa
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          ArrayOfUsers:
+ *                              type: array
+ *                              items:
+ *                                  type: object
+ *                                  properties:
+ *                                      id:
+ *                                          type: integer
+ *                                      name:
+ *                                          type: string
+ *                              example:
+ *                                  - id: 2333458458354
+ *                                    name: Job
+ */
 router.get('/', async (req, res) => {
     const peoples = await Person.find();
     res.json(peoples)
 });
 
+/**
+ * @swagger
+ * /add:
+ *  post:
+ *      summary: Inserta un usuario en la base de datos
+ *      description: Inserta un usuario en la base de datos
+ *      requestBody:
+ *          description: Usuario para agregar a la base de datos
+ *          content:
+ *                  application/json:
+ *                      schema:
+ *                          type: string
+ *          required: true
+ *      responses:
+ *          "201":
+ *              description: Usuario creado con éxito
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: integer
+ *          default:
+ *              description: Error inesperado
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: integer
+ */
 router.post('/add', async (req, res) => {
     const person = new Person(req.query);
     await person.save();
